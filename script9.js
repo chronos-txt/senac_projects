7467682309a51409f15c514131639b1c232cde79
chronos-txt <nicoladprpesce@hotmail.com>
2024-12-10 14:43:15 -0300

    Calculador Update

--- script9.js
+++ script9.js
@@ -1,52 +1,44 @@
+numButtons = document.querySelectorAll(".btn");
 
-numButtons = document.querySelectorAll(".btn")
-
-let num1 = ''
-let num2 = ''
-let operator = null
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let operators = ['+', '-', '/', '*']
+let result = '';
+let num1 = '';
+let num2 = '';
+let operator = null;
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let operators = ['+', '-', '/', '*'];
 
 numButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         let buttonValue = event.target.dataset.value;
-        
-        let currentValue
-        
-        if ( operators.includes(buttonValue)) {
-            operator = buttonValue
-            alert("op")
-        } else if (numbers.includes(parseInt(buttonValue))){
-            
+        let currentValue;
+
+        if (operators.includes(buttonValue)) {
+            operator = buttonValue;
+            update();
+        } else if (numbers.includes(parseInt(buttonValue))) {
             if (!operator) {
-                currentValue = buttonValue
+                currentValue = buttonValue;
                 num1 += buttonValue;
             } else {
-                num2 += buttonValue
-                console.log("Num2" + num2)
+                num2 += buttonValue;
+                console.log("Num2" + num2);
             }
-            console.log(num1, num2)
-
-            
+            console.log(num1, num2);
+            update();
         } else if (buttonValue == 'clear') {
-            
-            num1 = ''
-            num2 = ''
-            operator = null
-
+            num1 = '';
+            num2 = '';
+            operator = null;
+            result = ''; // Resetar o resultado
+            update();
         } else if (buttonValue == 'equals') {
-            
             if (num1 && num2 && operator) {
-                let result = calculate(parseFloat(num1), parseFloat(num2), operator)
-                alert(result)
-            }
-            
+                result = calculate(parseFloat(num1), parseFloat(num2), operator);
+                showResult();
+                
             }
         }
-
-    );
-
-    
+    });
 });
 
 function calculate(num1, num2, operator) {
@@ -64,9 +56,20 @@ function calculate(num1, num2, operator) {
     }
 }
 
-function update() {
+function update() { // Errado, conserte
     let display = document.getElementById("display");
+    
+    if (num2 !== '') {
+        display.value = num1 + ' ' + operator + ' ' + num2;
+    } else if (operator) {
+        display.value = num1 + ' ' + operator;
+    } else if (result != '') {
+        display.value = result;
+    } else {
+        display.value = num1;
+    }
+}
 
-    display.textContent = num1;
-
+function showResult() { // Gambiarra
+    display.value = result
 }
